40caccc97261880604ad2849ce7e3dad1d8a1b6e
Deven S <shriramsoft@gmail.com>
2024-04-11 12:47:02 +0530

    update for wordpress website

--- .devcontainer/wp-setup.sh
+++ .devcontainer/wp-setup.sh
@@ -3,7 +3,7 @@
 #Site configuration options
 SITE_TITLE="Dev Site"
 ADMIN_USER=admin
-ADMIN_PASS=password
+ADMIN_PASS=admin
 ADMIN_EMAIL="admin@localhost.com"
 #Space-separated list of plugin ID's to install and activate
 PLUGINS="advanced-custom-fields"
@@ -17,7 +17,7 @@ DEVDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd /var/www/html;
 if $WP_RESET ; then
     echo "Resetting WP"
-    wp plugin delete $PLUGINS
+  #  wp plugin delete $PLUGINS
     wp db reset --yes
     rm wp-config.php;
 fi
@@ -26,7 +26,7 @@ if [ ! -f wp-config.php ]; then
     echo "Configuring";
     wp config create --dbhost="db" --dbname="wordpress" --dbuser="wp_user" --dbpass="wp_pass" --skip-check;
     wp core install --url="http://localhost:8080" --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_email="$ADMIN_EMAIL" --admin_password="$ADMIN_PASS" --skip-email;
-    wp plugin install $PLUGINS --activate
+   # wp plugin install $PLUGINS --activate
     #TODO: Only activate plugin if it contains files - i.e. might be developing a theme instead
     wp plugin activate plugin-dev
 
@@ -36,10 +36,10 @@ if [ ! -f wp-config.php ]; then
         wp db import $f
     done
 
-    cp -r plugins/* /var/www/html/wp-content/plugins
-    for p in plugins/*; do
-        wp plugin activate $(basename $p)
-    done
+    #cp -r plugins/* /var/www/html/wp-content/plugins
+    #for p in plugins/*; do
+    #    wp plugin activate $(basename $p)
+    #done
 
 else
     echo "Already configured"
